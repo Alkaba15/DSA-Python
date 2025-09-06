0777f7ea9a9076d56a09aa5b6efebadf712b31be
Alseny Kaba <alsenykaba98@gmail.com>
2025-09-06 20:37:41 -0300

    implement and test the append method

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -13,8 +13,25 @@ class LinkedList :                  # LinkedList Class
          self.tail = None
          self.length = 0
 
+
     def print_list(self):
         temp = self.head
+        if temp is None :
+          print("None")
+          return
+
         while temp is not None:
             print(temp.value)
             temp = temp.next
+        
+
+    def append(self, value):
+       newNode = Node(value)
+       if self.head is None:
+          self.head = newNode
+          self.tail = newNode
+       else:
+          self.tail.next = newNode
+          self.tail = newNode
+          self.length+=1
+          
