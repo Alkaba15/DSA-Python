c39e93df7be45cbb75fa3c9b32448c48fddf8013
Alseny Kaba <alsenykaba98@gmail.com>
2025-09-22 22:52:24 -0300

    Merged branch linked_list to origin/main

--- LinkedList/linked_list.py
+++ LinkedList/linked_list.py
@@ -12,12 +12,13 @@ class LinkedList :                  # LinkedList Class
          self.head = None 
          self.tail = None
          self.length = 0
+         print('None')
 
 
     def print_list(self):
         temp = self.head
         if temp is None :
-          print("None")
+          print("Empty List")
           return
 
         while temp is not None:
@@ -34,4 +35,76 @@ class LinkedList :                  # LinkedList Class
           self.tail.next = newNode
           self.tail = newNode
           self.length+=1
-          
+
+    def pop(self):
+       if self.length == 0:
+          return None
+       temp = self.head
+       pre = self.head
+       
+       if self.length == 1:
+          self.head = None
+          self.tail = None
+          self.length-=1
+          return temp.value
+       
+       while(temp.next is not None):
+          pre = temp
+          temp = temp.next
+       self.tail = pre
+       self.tail.next = None
+       self.length-=1
+
+       return temp.value
+    
+    def prepend(self, value):
+      newNode = Node(value)
+      if self.length == 0:
+         self.head = newNode
+         self.tail = newNode
+         self.length = 1
+      else:
+         newNode.next = self.head
+         self.head = newNode
+         self.length+=1
+    
+    def pop_first(self):
+       if self.length == 0:
+          return None
+       temp = self.head
+       self.head = self.head.next
+       temp.next = None
+       self.length-=1
+       if self.length == 0:
+          self.tail = None
+       return temp.value
+    
+    def get(self, index):
+       if index < 0 or index >= self.length:
+          return None
+       temp = self.head
+
+       count = 0
+       while count < index:
+          temp = temp.next
+          count+=1
+       return temp.value 
+    
+    def set_value(self , index, value):
+       if index < 0 or index >= self.length:
+          raise Exception("Out of bonds")
+       else:
+          temp = self.head
+          for _ in range(index):
+             temp = temp.next
+          temp.value = value 
+       return temp.value
+    
+    def insert(self , index , value):
+       newNode = Node(value)
+       if index < 0 or index > self.length:
+          raise Exception("out of bounds")
+       if index == 0:
+          return self.prepend(value)
+       if index == self.length:
+          return self.append(value) 
--- LinkedList/test_linkedlist.py
+++ LinkedList/test_linkedlist.py
@@ -1,6 +1,7 @@
 #test LinkedList
 from linked_list import LinkedList
 
+<<<<<<< HEAD
 myList = LinkedList(20)     # New Linkedlist with one node -> value = 20
 <<<<<<< HEAD
 myList.print_list()         # print myList -> output -> 20
@@ -13,4 +14,14 @@ myList.print_list() # print myList -> output -> 20
 myList.append(30)            # Append new value-> 30 to myList 
 myList.print_list()  # New output -> 20 30
 >>>>>>> 0777f7ea9a9076d56a09aa5b6efebadf712b31be
+=======
+myList =  LinkedList(10)
+myList.append(20)
+myList.append(30)
+myList.append(40)
+myList.append(50)
+myList.append(60)
+>>>>>>> linked_list
 
+myList.set_value(6,200)
+myList.print_list()
