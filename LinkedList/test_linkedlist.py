2b6c6fd338d9fbbbacbced2d2b7e01ae92deeb4c
Alseny Kaba <alsenykaba98@gmail.com>
2025-09-23 23:35:09 -0300

    Completing implementation of Insert method

--- LinkedList/test_linkedlist.py
+++ LinkedList/test_linkedlist.py
@@ -1,12 +1,9 @@
 #test LinkedList
 from linked_list import LinkedList
 
-myList =  LinkedList(10)
-myList.append(20)
-myList.append(30)
-myList.append(40)
-myList.append(50)
-myList.append(60)
-
-myList.set_value(6,200)
-myList.print_list()
+myList = LinkedList(20)     # New Linkedlist with one node -> value = 20
+myList.insert(0,10)
+myList.insert(1, 30)
+myList.insert(0,0)
+myList.insert(4,40)
+myList.print_list()         # print myList -> output -> 0 , 10 , 30 , 20 , 40
