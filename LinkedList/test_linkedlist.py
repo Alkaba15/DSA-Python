ac75082f17f28cf67f191b7cce500b64446ac6fc
Alseny Kaba <alsenykaba98@gmail.com>
2025-09-23 23:56:02 -0300

    Supprime les anciennes lignes de test

--- LinkedList/test_linkedlist.py
+++ LinkedList/test_linkedlist.py
@@ -7,11 +7,6 @@ myList.insert(1, 30)
 myList.insert(0,0)
 myList.insert(4,40)
 myList.print_list()         # print myList -> output -> 0 , 10 , 30 , 20 , 40
-myList.append(20)
-myList.append(30)
-myList.append(40)
-myList.append(50)
-myList.append(60)
 
 myList.set_value(6,200)
 myList.print_list()
